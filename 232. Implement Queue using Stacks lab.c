56a3e853c65cc9456b158ce8cac641978f289dab
miliocoding <113136371+miliocoding@users.noreply.github.com>
2025-03-10 17:35:13 -0500

    Update 232. Implement Queue using Stacks lab.c

--- 232. Implement Queue using Stacks lab.c
+++ 232. Implement Queue using Stacks lab.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+typedef struct {
+    int *stack1;
+    int *stack2;
+    int top1;
+    int top2;
+    int capacity;
+} MyQueue;
+
+MyQueue* myQueueCreate() {
+    MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+    obj->capacity = 100;
+    obj->stack1 = (int*)malloc(obj->capacity * sizeof(int));
+    obj->stack2 = (int*)malloc(obj->capacity * sizeof(int));
+    obj->top1 = -1;
+    obj->top2 = -1;
+    return obj;
+}
+
+void myQueuePush(MyQueue* obj, int x) {
+    if (obj->top1 == obj->capacity - 1) {
+        obj->capacity *= 2;
+        obj->stack1 = (int*)realloc(obj->stack1, obj->capacity * sizeof(int));
+    }
+    obj->stack1[++obj->top1] = x;
+}
+
+int myQueuePop(MyQueue* obj) {
+    if (obj->top2 == -1) {
+        while (obj->top1 != -1) {
+            obj->stack2[++obj->top2] = obj->stack1[obj->top1--];
+        }
+    }
+    return obj->stack2[obj->top2--];
+}
+
+int myQueuePeek(MyQueue* obj) {
+    if (obj->top2 == -1) {
+        while (obj->top1 != -1) {
+            obj->stack2[++obj->top2] = obj->stack1[obj->top1--];
+        }
+    }
+    return obj->stack2[obj->top2];
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+    return (obj->top1 == -1 && obj->top2 == -1);
+}
+
+void myQueueFree(MyQueue* obj) {
+    free(obj->stack1);
+    free(obj->stack2);
+    free(obj);
+}
